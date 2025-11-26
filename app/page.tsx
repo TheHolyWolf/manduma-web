5f9f16ffae345ce6003d971f3c37e2aa0d1ce785
Youssef Boulazrag <boulazragyoussef@gmail.com>
2025-11-26 20:57:19 +0100

    First save: Welcome page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white">
+      <h1 className="text-4xl font-bold text-blue-900">
+        Welcome to Manduma
+      </h1>
+      <p className="text-gray-600 mt-4 text-xl">
+        Innovating the future.
+      </p>
+    </main>
+  );
+}
